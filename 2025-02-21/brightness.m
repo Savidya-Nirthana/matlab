3f311700c7d16e365f9d4bf3dd75e8154cd4ac1c
Savidya Perera <136046765+Savidya-Nirthana@users.noreply.github.com>
2025-04-22 18:56:16 +0530

    Update brightness.m

--- 2025-02-21/brightness.m
+++ 2025-02-21/brightness.m
@@ -1,21 +1,22 @@
-function I = brightness(I, c)
-    if length(size(I)) == 3
-        I = rgb2gray(I);
-    end
-    
-    [row, col] = size(I);
-    for x=1:row
-        for y=1: col
-            a = I(x, y);
-            a = a+c;
-            if a<0
-                a = 0;
-            end
-            if a>255
-                a = 255;
-            end
+function I = brightness(I, value)
 
-        end
-    end
-            
-                
+if length(size(I)) == 3
+    I = rgb2gray(I);
+end
+
+[M,N] = size(I);
+
+ for i=1:M
+     for j=1:N
+         a = I(i, j);
+         a = a + value;
+         if a > 255
+             a = 255;
+         elseif a<0
+             a = 0;
+         end
+         I(i,j) = a;
+     end
+ end
+
+
