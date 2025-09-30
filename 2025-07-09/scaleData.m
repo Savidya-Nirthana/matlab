f21439ead6f1f5cf80bccc9ae3bbdba737486bb8
Savidya Perera <136046765+Savidya-Nirthana@users.noreply.github.com>
2025-09-30 09:57:34 +0530

    Update scaleData.m

--- 2025-07-09/scaleData.m
+++ 2025-07-09/scaleData.m
@@ -5,9 +5,10 @@ function [train, test] = scaleData(train, test)
     normalMin = -1;
     
     for i=1: (col-1)
-         realMax = max(train(:, 1));
+         realMax = max(train(:, i));
          realMin = min(train(:, i));
          ratio_scale = (normalMax- normalMin)/(realMax - realMin);
          train(:, i) = (ratio_scale * (train(:,i) - realMin)) + normalMin;
          test(:,i) = (ratio_scale*(test(:,i) - realMin)) + normalMin;
     end
+
